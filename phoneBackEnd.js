a87d7a652a38ef7bcb0001475baa67b5b49fc7a8
OscarLui_iOs <88272090+shieluyyoa@users.noreply.github.com>
2023-03-07 01:28:32 +0800

    uibuild

--- phoneBackEnd.js
+++ phoneBackEnd.js
@@ -7,6 +7,7 @@ morgan.token('body', function(req,res) {
     console.log(req.body)
     return JSON.stringify(req.body);
 })
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
